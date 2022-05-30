ab28b3d8112562ea2a50c9a2eac044da4b4b6f0a
Fariz <mana.freeze@gmail.com>
2022-05-30 22:15:01 +0700

    Add file not exists prevention for unit testing only.

--- name-sorter-test/UnitTest1.cs
+++ name-sorter-test/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Xunit;
 
@@ -10,6 +11,27 @@ namespace name_sorter_test
         [Fact]
         public void Test1()
         {
+            string inputFileName = "unsorted-names-list.txt";
+            string input = @"Orson Milka Iddins
+Erna Dorey Battelle
+Flori Chaunce Franzel
+Odetta Sue Kaspar
+Roy Ketti Kopfen
+Madel Bordie Mapplebeck
+Selle Bellison
+Leonerd Adda Mitchell Monaghan
+Debra Micheli
+Hailey Avie Annakin";
+
+            //if the test file not exists, create it
+            if (!System.IO.File.Exists(inputFileName))
+            {
+                var inputs = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+                using (StreamWriter outputFile = new StreamWriter(inputFileName))
+                    foreach (string s in inputs)
+                        outputFile.WriteLine(s);
+            }
+
             string expectedResult = @"Hailey Avie Annakin
 Erna Dorey Battelle
 Selle Bellison
@@ -20,9 +42,10 @@ Roy Ketti Kopfen
 Madel Bordie Mapplebeck
 Debra Micheli
 Leonerd Adda Mitchell Monaghan";
+
             List<string> expectedList = expectedResult.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
             List<string> list;
-            string res = name_sorter.Helper.SortNameAndSave("unsorted-names-list.txt", out list);
+            string res = name_sorter.Helper.SortNameAndSave(inputFileName, out list);
             Assert.NotNull(list);
             Assert.Equal(expectedList, list);
         }
